b341c2ca609f59cf6ca694f0dee0b11cadad13d1
Aasiy Zabeen <syedasiya000@gmail.com>
2020-09-13 17:32:52 +0530

    add passport passport-local-mongoose seccion

--- app.js
+++ app.js
@@ -6,22 +6,35 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 // const encrypt = require('mongoose-encryption');
 // const md5 = require('md5');
-const bcrypt = require('bcrypt');
-const saltRound = 5;
+// const bcrypt = require('bcrypt');
+// const saltRound = 5;
+const session = require('express-session');
+const passport = require('passport');
+const passportlocalmongoose = require('passport-local-mongoose');
 
 
 const app = express();
-//let posts=[];
-app.set('view engine', 'ejs');
 
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+
+app.use(session({
+  secret: 'Our little secret .',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
+mongoose.set("useCreateIndex", true);
 
 // schema for User db in simple js object
 // const userSchema={
@@ -34,6 +47,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+userSchema.plugin(passportlocalmongoose);
 //=====using key encriptions of mongoose-encryption
 // const secret = "AnyLongStringToCeepMySecret";
 // userSchema.plugin(encrypt,{secret:secret, encryptedFields:['password']});
@@ -43,6 +58,9 @@ const userSchema = new mongoose.Schema({
 // userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
 
 const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -58,85 +76,10 @@ app.get("/register", function(req, res) {
 
 app.post("/register", function(req, res) {
 
-  bcrypt.hash(req.body.username, saltRound, function(err, hash) {
-    //store hash
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-    console.log(hash);
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
-
-  // console.log(req.body.username);
-  // const newUser = new User( {
-  //     email:req.body.username,
-  //     password:md5(req.body.password)
-  //   });
-  //   newUser.save(function(err){
-  //     if (err) {
-  //       console.log(err);
-  //     } else {
-  //       res.render("secrets");
-  //     }
-  //   });
-
-  // const username = req.boy.username;
-  // console.log(username);
-  // const password = req.body.password;
-  // // we only let user inn main page(secrets) if they registered first
-  // if (!password && !username) {
-  //   console.log("enter fieldes");
-  // }else {
-  //   const newUser = new User( {
-  //     email:username,
-  //     password:password
-  //   });
-  //   newUser.save(function(err){
-  //     if (err) {
-  //       console.log(err);
-  //     } else {
-  //       res.render("secrets");
-  //     }
-  //   });
-  // }
 });
 
 app.post("/login", function(req, res) {
-  const username = req.body.username;
-  // console.log(username);
-  const password = req.body.password;
-  console.log(password);
-  User.findOne({
-    email: username
-  }, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        // if(foundUser.password === password){
-        //   res.render("secrets");
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          //result==true
-          console.log(foundUser.password);
-          console.log(password);
-          console.log(result);
-          if (result === true) {
-            res.render("secrets");
-          } else {
-            console.log("not found");
-          }
-        });
-      }
-    }
 
-  });
 });
 
 app.listen("3000", function() {
