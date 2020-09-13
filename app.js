a3b057068c31fabf593c4b692796aadb807274da
Aasiy Zabeen <syedasiya000@gmail.com>
2020-09-13 08:18:14 +0530

    level-4  bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 // const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRound = 5;
 
 
 const app = express();
@@ -17,7 +19,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 // schema for User db in simple js object
 // const userSchema={
@@ -26,9 +30,9 @@ mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 // };
 
 // schem for User Db in proper mongoose userSchema
-const userSchema= new mongoose.Schema({
-  email:String,
-  password:String
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
 });
 //=====using key encriptions of mongoose-encryption
 // const secret = "AnyLongStringToCeepMySecret";
@@ -38,33 +42,50 @@ const userSchema= new mongoose.Schema({
 // ==== using .env encryption
 // userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
 
-const User= new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register",function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register",function(req,res){
-// console.log(req.body.username);
-  const newUser = new User( {
-      email:req.body.username,
-      password:md5(req.body.password)
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.username, saltRound, function(err, hash) {
+    //store hash
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
     });
-    newUser.save(function(err){
+    console.log(hash);
+    newUser.save(function(err) {
       if (err) {
         console.log(err);
       } else {
         res.render("secrets");
       }
     });
+  });
+
+  // console.log(req.body.username);
+  // const newUser = new User( {
+  //     email:req.body.username,
+  //     password:md5(req.body.password)
+  //   });
+  //   newUser.save(function(err){
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       res.render("secrets");
+  //     }
+  //   });
 
   // const username = req.boy.username;
   // console.log(username);
@@ -87,23 +108,37 @@ app.post("/register",function(req,res){
   // }
 });
 
-app.post("/login",function(req,res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
   // console.log(username);
-  const password = md5(req.body.password);
-  User.findOne({email:username},function(err,foundUser){
-    if(err){
+  const password = req.body.password;
+  console.log(password);
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        // if(foundUser.password === password){
+        //   res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          //result==true
+          console.log(foundUser.password);
+          console.log(password);
+          console.log(result);
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            console.log("not found");
+          }
+        });
       }
     }
+
   });
 });
 
-app.listen("3000",function(){
+app.listen("3000", function() {
   console.log("server runs at 3000");
 });
